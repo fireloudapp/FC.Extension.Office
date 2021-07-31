507b49598448aa9076460ba415de6f1ac93fe898
Ganeshram Ravi <ganeshram.ravi@rrd.com>
2021-07-31 16:15:14 +0530

    Added Convert List to Excel Test Method

--- Source/FC.Extension.Office.Test/ExcelHanders/ConvertToListFromExcelTest.cs
+++ Source/FC.Extension.Office.Test/ExcelHanders/ConvertToListFromExcelTest.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
 using Xunit;
+using FC.Core.Extension.Office.ExcelHandlers;
+using FC.Extension.Office.Test.Samples;
+using System.IO;
+using Shouldly;
 
 namespace FC.Extension.Office.Test.ExcelHanders
 {
@@ -16,8 +20,32 @@ namespace FC.Extension.Office.Test.ExcelHanders
             this._output = output;
         }
 
-        #region Encrypt or Decrypt Test
-        
+        #region Convert to Excel
+        [Fact]
+        public void ToListToExcel_Test()
+        {
+            List<SampleExcelModel> dataList = new List<SampleExcelModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dataList.Add(new SampleExcelModel()
+                {
+                    Item = "Teest " + i,
+                    OrderData = DateTime.Now.AddDays(i),
+                    Region = "IN",
+                    Rep = "Got",
+                    UnitCost = 50 * i,
+                    Units = 5,
+                    Total = (50 * i) * 5
+                });
+            }
+            dataList.ToExcel("Sample.xlsx");
+            bool result = File.Exists("Sample.xlsx");
+            result.ShouldBeTrue();
+            dataList.ShouldNotBeNull();
+
+            _output.WriteLine($"List to Excel Length Lenght : {dataList.Count}");
+        }
         #endregion
     }
 }
